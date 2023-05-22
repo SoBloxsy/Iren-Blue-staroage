48f6bc2e19906d51cf80e3fa92eedec617de0d53
Sobloxsy <45794556+SoBloxsy@users.noreply.github.com>
2023-05-22 02:52:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome to Iren Blue.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="We're not just ready yet!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          but we will be #soon</code>
         </p>
       </main>
 
